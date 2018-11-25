b042d8faf288e8929a2c2882c3060d9f2725b748
Future-Fly <44969741+Future-Fly@users.noreply.github.com>
2018-11-25 22:14:32 +0900

    Update project.py

--- project.py
+++ project.py
@@ -1,1 +1,13 @@
-import Numpy
+import numpy as np
+import pandas as pd
+import gc
+import warnings
+from sklearn.feature_selection import SelectFromModel
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import OneHotEncoder
+
+
+warnings.filterwarnings("ignore")
+application_train = pd.read_csv('data_train.csv')
+catFeatures = [4, 5, 6, 7, 13, 17]
