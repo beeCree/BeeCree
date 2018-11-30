27398540fbb13935553570c156fd68d20ff1bfb2
Future-Fly <44969741+Future-Fly@users.noreply.github.com>
2018-12-01 04:06:01 +0900

    Update project.py

--- project.py
+++ project.py
@@ -4,6 +4,7 @@ import gc
 import warnings
 import time
 import sys
+import os
 import math
 from sklearn.feature_selection import SelectFromModel
 from sklearn.ensemble import RandomForestClassifier
@@ -19,10 +20,11 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import MinMaxScaler
 FILE_NAME =1
 MAKEY_SKIP = 2
-ONEHOT_SKIP = 3
-IMPUTE_SKIP = 4
-KIND_OF_FS = 5
-M_FEATURES =50
+KIND_OF_ENC = 3
+ENCODER_SKIP = 4
+IMPUTE_SKIP = 5
+KIND_OF_FS = 6
+M_FEATURES =20
 CONST_DATE_INDEX = 13
 ##**************************************************************************************
 ## 1. declaritive coding style로 더 보기 편하게 만들었음
@@ -63,6 +65,8 @@ def length(array1): ## given ndarray, returns int
     return array1.shape[0]
 def width(array1): ## given ndarray, returns int
     return array1.shape[1]
+def filename():
+    return sys.argv[FILE_NAME][:-4]+'/'+sys.argv[KIND_OF_ENC]
 def dateColumn2Float(dataframe, col_index): ## given dataframe and int, returns dataframe
     array1 = dataframe.values
     for i in range(length(array1)):
@@ -79,7 +83,7 @@ def dateColumn2Float(dataframe, col_index): ## given dataframe and int, returns 
 def makey(application_train):
     print("Making y...")
     if(sys.argv[MAKEY_SKIP] == "Y"):
-        y = pd.read_csv('y.csv')
+        y = pd.read_csv(filename()+'/y.csv')
         print("y=")
         print(y)
         gc.collect()
@@ -89,14 +93,80 @@ def makey(application_train):
         application_train.drop("price", axis=1, inplace=True)
         print("y=")
         print(y)
-        y.to_csv("y.csv", index=False)
+        y.to_csv(filename()+'/y.csv', index=False)
         gc.collect()
         return y
-def onehotNd2f(application_train, y): ## input이 "skip"이면 skip. else not skip
-    print("Reading data_train.csv and One-Hot encoder and imputing...")
-    if(sys.argv[ONEHOT_SKIP] == "Y"):
+def enc_selector(application_train, y):
+    if(sys.argv[KIND_OF_ENC] == "bin"):
+        print("Binary encoder selected.")
+        return binaryNd2f(application_train, y)
+    if(sys.argv[KIND_OF_ENC] == "ohc"):
+        print("One hot encoder selected.")
+        return onehotNd2f(application_train, y)
+def binaryNd2f(application_train, y):
+    if(sys.argv[ENCODER_SKIP] == "Y"):
+        print("Skipping binary encoder")
         gc.collect()
-        total_data_df = pd.read_csv('concat.csv')
+        total_data_df = pd.read_csv(filename()+'/concat_b.csv')
+        return total_data_df
+    print(sys.argv[FILE_NAME][:-4])
+    print("Executing binary encoder..")
+    gc.collect()
+    cat_features = [4, 5, 6, 7, 14, 18]
+    non_cat_features = []
+    for i in range(application_train.shape[1]):
+        if i in cat_features:
+            pass
+        else:
+            non_cat_features.append(i)
+    # 그냥 manual하게 binary encoding을 구현함
+    # recover을 어떻게 할 것인가? max min을 저장해놓음 되지 않을까?
+    cat_max = []
+    cat_min = []
+    dims = []
+    for i in cat_features:
+        temp_max = -123456
+        temp_min = 123456
+        for j in range(length(application_train)):
+            if(np.isnan(application_train.iloc[j, i])):
+                pass
+            else:
+                if(temp_max < application_train.iloc[j, i]):
+                    temp_max = application_train.iloc[j, i]
+                if(temp_min > application_train.iloc[j, i]):
+                    temp_min = application_train.iloc[j, i]
+        cat_max.append(temp_max)
+        cat_min.append(temp_min)
+        dims.append(int(math.ceil(np.log2(temp_max-temp_min+1))))
+    print(cat_min)
+    non_cat_train = application_train.iloc[:,non_cat_features].values
+    for i in range(6):
+        binencoded = np.zeros((length(application_train),dims[i]))
+        for j in range(length(application_train)):
+            temp = application_train.iloc[j, cat_features[i]]-cat_min[i]
+            print(temp)
+            for k in range(int(math.ceil(dims[i]))):
+                binencoded[j, k] = temp%2
+                if(np.isnan(temp)):
+                    pass
+                else:
+                    temp = math.floor(temp/2)
+        non_cat_train = np.concatenate((non_cat_train, binencoded), axis=1)
+    bin_concated=pd.DataFrame(non_cat_train)
+    bin_concated = dateColumn2Float(bin_concated, 0) ##0번째 feature은 날짜.
+        
+    bin_concated = dateColumn2Float(bin_concated, CONST_DATE_INDEX)
+    ##원래 18번째 feature가 날짜지만 카테고리데이터가 뒤로 가서 12번째로 당겨짐
+    ##항상 12번째로 오므로 딱히 문제는 없다고 생각
+    print("done.")
+    bin_concated.to_csv(filename()+'/concat_b.csv', index=False)
+    print(bin_concated)
+    return bin_concated
+
+def onehotNd2f(application_train, y): ## input이 "skip"이면 skip. else not skip
+    if(sys.argv[ENCODER_SKIP] == "Y"):
+        gc.collect()
+        total_data_df = pd.read_csv(filename()+'/concat_o.csv')
         return total_data_df
     else:
         gc.collect()
@@ -131,8 +201,8 @@ def onehotNd2f(application_train, y): ## input이 "skip"이면 skip. else not sk
         ##항상 12번째로 오므로 딱히 문제는 없다고 생각
         print("done.")
         print(total_data_df)
-        print("Saving data on concat.csv")
-        total_data_df.to_csv("concat.csv", index=False)
+        print("Saving data on concat_o.csv")
+        total_data_df.to_csv(filename()+'/concat_o.csv', index=False)
         return total_data_df
         ## one hot encoding finish
         ## *************************************************************
@@ -140,7 +210,7 @@ def impute(total_data_df):
     print("Imputing...")
     if(sys.argv[IMPUTE_SKIP] == "Y"):
         gc.collect()
-        total_data_df = pd.read_csv('impute.csv')
+        total_data_df = pd.read_csv(filename()+'/impute.csv')
         return total_data_df
     else:
         gc.collect()
@@ -148,7 +218,7 @@ def impute(total_data_df):
         print("done.")
         print(total_data_df)
         print("Saving data on impute.csv")
-        pd.DataFrame(total_data_df).to_csv("impute.csv", index=False)
+        pd.DataFrame(total_data_df).to_csv(filename()+'/impute.csv', index=False)
         return total_data_df
 def selectfs(total_data_df,y):
     run_time_list=[]
@@ -178,7 +248,7 @@ def selectfs(total_data_df,y):
         end=time.time()
         run_time_list.append(end-start)
 
-    pd.DataFrame(run_time_list).to_csv("run_time.csv", index=False)
+    pd.DataFrame(run_time_list).to_csv(filename()+'/run_time.csv', index=False)
     return 1;
 def wrapper(total_data_df,y):
     print("Wrapping...")
@@ -188,7 +258,7 @@ def wrapper(total_data_df,y):
     rfe_support = rfe_selector.get_support()
     rfe_feature = total_data_df.loc[:,rfe_support].columns.tolist()
     print(str(len(rfe_feature)), 'selected features')
-    pd.DataFrame(rfe_feature).to_csv("wrapper.csv", index=False)
+    pd.DataFrame(rfe_feature).to_csv(filename()+'/wrapper.csv', index=False)
     return rfe_feature
 
 def lightGBM(total_data_df,y):
@@ -199,7 +269,7 @@ def lightGBM(total_data_df,y):
     sfm_support = sfm.get_support()
     sfm_feature = total_data_df.loc[:,sfm_support].columns.tolist()
     print(str(len(sfm_feature)), 'selected features')
-    pd.DataFrame(sfm_feature).to_csv("lightgbm.csv", index=False)
+    pd.DataFrame(sfm_feature).to_csv(filename()+'/lightgbm.csv', index=False)
     return sfm_feature
 
 def cor_selector(X, y):
@@ -215,7 +285,7 @@ def cor_selector(X, y):
     cor_feature = X.iloc[:,np.argsort(np.abs(cor_list))[-M_FEATURES:]].columns.tolist()
     cor_feature.sort()
     print(str(len(cor_feature)), 'selected features')
-    pd.DataFrame(cor_feature).to_csv("pearson.csv", index=False)
+    pd.DataFrame(cor_feature).to_csv(filename()+'/pearson.csv', index=False)
     return cor_feature
 
 def chi2_square(total_data_df, y):
@@ -226,7 +296,7 @@ def chi2_square(total_data_df, y):
     chi_support = chi_selector.get_support()
     chi_feature = total_data_df.loc[:,chi_support].columns.tolist()
     print(str(len(chi_feature)), 'selected features')
-    pd.DataFrame(chi_feature).to_csv("chi2.csv", index=False)
+    pd.DataFrame(chi_feature).to_csv(filename()+'/chi2.csv', index=False)
     return chi_feature
 
 def Embedded_LR(total_data_df,y):
@@ -240,9 +310,10 @@ def Embedded_LR(total_data_df,y):
     embedded_lr_support = embedded_lr_selector.get_support()
     embedded_lr_feature = X.loc[:,embedded_lr_support].columns.tolist()
     print(str(len(embedded_lr_feature)), 'selected features')
-    pd.DataFrame(embedded_lr_feature).to_csv("embedded_lr.csv", index=False)
+    pd.DataFrame(embedded_lr_feature).to_csv(filename()+'/embedded_lr.csv', index=False)
     return embedded_lr_selector.get_support(indices = True)
-
+if(not os.path.exists(filename())):
+    os.makedirs(filename())
 gc.collect()        
 warnings.filterwarnings("ignore")
 ## input data에서 첫 줄을 띄어줘야 한다.
@@ -254,7 +325,7 @@ y=makey(application_train)
 
 application_train = handle_deg(application_train)
 
-total_data_df = onehotNd2f(application_train, y)
+total_data_df = enc_selector(application_train, y)
 gc.collect()
 
 total_data_df = pd.DataFrame(impute(total_data_df))
