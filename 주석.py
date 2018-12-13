0de7294f6901d2de9eaaa82464e128b254a4839e
Future-Fly <44969741+Future-Fly@users.noreply.github.com>
2018-12-14 03:50:15 +0900

    Update 주석.py

--- 주석.py
+++ 주석.py
@@ -3,6 +3,7 @@ import pandas as pd
 import gc
 import warnings
 import sys
+import time
 import os
 import math
 from math import isnan
@@ -16,7 +17,7 @@ ENCODER_SKIP = 4 ## encoder가 이미 실행 된 경우 skip할 지 결정
 IMPUTE_SKIP = 5 ## impute이 이미 실행 된 경우 skip할 지 결정
 M_FEATURES = 20
 CONST_DATE_INDEX = 13
-
+TREE_DEPTH = 3
 def log_y(y): ##y 행렬 원소들 각각 log
     logy = np.zeros((length(y),1))
     for i in range(length(y)):
@@ -87,23 +88,23 @@ def makey(application_train):
         gc.collect()
         return y
 
-def enc_selector(application_train, y):
+def enc_selector(application_train):
     ## categorical data를 handle하기 위해서, 어떤 encoder를 사용할 지 선택하는 함수였지만,
-    ## 이를 모델에 적용시켰을 때, train error가 비슷했기 때문에 그냥 dimension이 가장 작아서
-    ## overfitting이 적게 encoder를 쓰지 않게 했다.
+    ## 이를 모델에 적용시켰을 때, binary encoder를 사용했을 때의 train error가 가장 작아서
+    ## binary encoder만을 사용하기로 했다.
     if(sys.argv[KIND_OF_ENC] == "bin"):
         print("Binary encoder selected.")
-        return binaryNd2f(application_train, y)
-    if(sys.argv[KIND_OF_ENC] == "ohc"):
-        print("One hot encoder selected.")
-        return onehotNd2f(application_train, y)
+        return binaryNd2f(application_train)
     if(sys.argv[KIND_OF_ENC] == "non"):
         print("Not encoding selected")
-        return notEncode(application_train,y)
+        return notEncode(application_train)
         
-def binaryNd2f(application_train, y):
-    ## unused
-    ## categorical data의 value를 이진수로 나타내서 각 자리를 한 feature로 나타내어 encode했다.
+def binaryNd2f(application_train):
+    ## categorical data의 각각의 value에 그 column의 최소 값을 빼서
+    ## 이를 이진법으로 나타내고(당연히 categorical data는 모두 integer value를 가진다)
+    ## 각 자리와 그 자리수를 feature 한 줄과 그 value로 바꾸는 encoding이다.
+    ## ex) 이 column의 최소값이 3, 최대값이 8, i번째 row의 값이 6이면
+    ##      6 - 3 = 011으로 바꾸어, 0, 1, 1로 바꾼다.
     if(sys.argv[ENCODER_SKIP] == "Y"):
         print("Skipping binary encoder")
         gc.collect()
@@ -159,53 +160,7 @@ def binaryNd2f(application_train, y):
     print(bin_concated)
     return bin_concated
 
-def onehotNd2f(application_train, y): 
-    if(sys.argv[ENCODER_SKIP] == "Y"): ## input이 "skip"이면 skip. else not skip
-        gc.collect()
-        total_data_df = pd.read_csv(filename()+'/concat_o.csv')
-        return total_data_df
-    else:
-        gc.collect()
-        ## handle degree
-        application_train = handle_deg(application_train)
-        ## *********************************************************
-        cat_features = [4, 5, 6, 7, 14, 18] ## 실제 데이터에서 카테고리 데이터의 인덱스
-        non_cat_features = []
-        for i in range(application_train.shape[1]):
-            if i in cat_features:
-                pass
-            else:
-                non_cat_features.append(i)
-        ## 카테고리 데이터와 아닌 데이터들의 인덱스 분류
-        ## ********************************************************
-        print("Executing One-Hot encoder...")
-        onehot_encoder = OneHotEncoder(handle_unknown='ignore')
-        application_train=application_train.replace(float('nan'), -123)
-        onehot_encoded = onehot_encoder.fit_transform(application_train.iloc[:,cat_features]).toarray()
-        print("done.")
-        print(onehot_encoded)
-        
-        print("Concatenating Array...")
-        non_cat_data = application_train.iloc[:,non_cat_features]
-        total_data_nd = np.concatenate((non_cat_data, onehot_encoded), axis=1)
-        total_data_df = pd.DataFrame(total_data_nd).replace(-123,float('nan'))
-        
-        print("done.\n")
-        print("Converting data to float and reproducing NaNs...")
-        total_data_df = dateColumn2Float(total_data_df, 0) ##0번째 feature은 날짜.
-        
-        total_data_df = dateColumn2Float(total_data_df, CONST_DATE_INDEX)
-        ##원래 18번째 feature가 날짜지만 카테고리데이터가 뒤로 가서 12번째로 당겨짐
-        ##항상 12번째로 오므로 딱히 문제는 없다고 생각
-        print("done.")
-        print(total_data_df)
-        print("Saving data on concat_o.csv")
-        total_data_df.to_csv(filename()+'/concat_o.csv', index=False)
-        return total_data_df
-        ## one hot encoding finish
-        ## *************************************************************
-
-def notEncode(application_train, y):
+def notEncode(application_train):
     ## encoding을 하지 않고, given data의 날짜를 float로 고치기만 하는 함수.
     if(sys.argv[ENCODER_SKIP] == "Y"):
         print("Skipping binary encoder")
@@ -244,16 +199,16 @@ def impute(total_data_df):
 def gbr(total_data_df, y, testx, testy):
     ## make a model using gradient boosting regressor tree
     print("gradientboostingregressor")
-    ## maximum depth of tree is 9
-    GBR = GradientBoostingRegressor(n_estimators = 100, max_depth = 5)
-    GBR.fit(total_data_df,log_y(y))
-    acc=accuracy(testy,exp_y(GBR.predict(testx)))
+    ## maximum depth of tree is 10
+    GBR = GradientBoostingRegressor(n_estimators = 100, max_depth = TREE_DEPTH)
+    GBR.fit(total_data_df,log_y(y)) ## y의 log를 취한다.
+    acc=accuracy(testy,exp_y(GBR.predict(testx))) ## 다시 exp로 원래 값으로 복원한다.
     print("Accuracy --> ", acc)
     return acc
 
 def accuracy(real, pred):
     ## calculate accuracy between real data and predicted data
-    ## accuracy is calculated as same as slide
+    ## accuracy is calculated as same as homework slide
     total = 0
     for i in range(len(real)):
         total=total+abs((real[i]-pred[i])/real[i])
@@ -295,6 +250,15 @@ def fold5(total_data_df, y):
     acc3 = gbr(X[listn3,:], ynd[listn3,:],X[list3,:],ynd[list3,:])
     acc4 = gbr(X[listn4,:], ynd[listn4,:],X[list4,:],ynd[list4,:])
     print((acc0+acc1+acc2+acc3+acc4)/5)
+def predict(train_x, y):
+    ## 전체 data_train.csv의 값으로 GradientBoostingRegressor model을 만들고
+    ## data_test.csv에 이를 적용한다.
+    test_x = pd.read_csv("data_test.csv")
+    GBR = GradientBoostingRegressor(n_estimators = 100, max_depth = TREE_DEPTH)
+    GBR.fit(train_x,log_y(y.values)) ## y의 log를 취한다.
+    test_data = exp_y(GBR.predict(impute(enc_selector(test_x)))) ## exp로 원래 값으로 복원한다.
+    pd.DataFrame(test_data).to_csv(filename()+'/return.csv', index=False)
+start = time.time()
 if(not os.path.exists(filename())):
     os.makedirs(filename())
 gc.collect()        
@@ -303,9 +267,13 @@ warnings.filterwarnings("ignore")
 application_train = pd.read_csv(sys.argv[FILE_NAME])
 y=makey(application_train)
 
-total_data_df = enc_selector(application_train, y)
+total_data_df = enc_selector(application_train)
 gc.collect()
 
 total_data_df = pd.DataFrame(impute(total_data_df))
 gc.collect()
 fold5(total_data_df, y)
+predict(total_data_df, y)
+end = time.time()
+run_time = end - start
+print(run_time)
